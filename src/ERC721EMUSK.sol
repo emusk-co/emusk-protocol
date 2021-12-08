f444d7139933b34f99098a5a155525291bad5524
ruymaster <mecury1984@outlook.com>
2021-12-09 01:07:14 +0800

    add sign for erc721

--- src/ERC721EMUSK.sol
+++ src/ERC721EMUSK.sol
@@ -2,23 +2,24 @@
 pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
 import "./common/Ownable.sol";
+import "./common/roles/SignerRole.sol";
 import "./interfaces/erc721/IERC721.sol";
-// import "./interfaces/erc721/IERC721Metadata.sol";
 import "./token/erc721/ERC721Burnable.sol";
 import "./token/erc721/ERC721Base.sol";
 /**
  * @title ERC721EMUSK
  * @dev anyone can mint token.
  */
-contract ERC721EMUSK is Ownable, IERC721, ERC721Burnable, ERC721Base {
+contract ERC721EMUSK is Ownable, SignerRole, IERC721, ERC721Burnable, ERC721Base {
 
-    constructor (string memory _name, string memory _symbol, address newOwner, string memory contractURI, string memory tokenURIPrefix) ERC721Base(_name, _symbol, contractURI, tokenURIPrefix) {
+    constructor (string memory _name, string memory _symbol, address signer, string memory contractURI, string memory tokenURIPrefix) ERC721Base(_name, _symbol, contractURI, tokenURIPrefix) {
+        _addSigner(signer);
         _registerInterface(bytes4(keccak256("MINT_WITH_ADDRESS")));
-        transferOwnership(newOwner);
     }
 
     function mint(uint256 tokenId, uint8 v, bytes32 r, bytes32 s, Fee[] memory _fees, string memory _tokenURI) public {
-        require(owner() == ecrecover(keccak256(abi.encodePacked(this, tokenId)), v, r, s), "owner should sign tokenId");
+        // require(isSigner(ecrecover(toEthSignedMessageHash(keccak256(abi.encodePacked(this, tokenId))), v, r, s)), "owner should sign tokenId");
+        require(isSigner(ecrecover(keccak256(abi.encodePacked(this, tokenId)), v, r, s)), "owner should sign tokenId");
         _mint(msg.sender, tokenId, _fees);
         _setTokenURI(tokenId, _tokenURI);
     }
@@ -58,5 +59,21 @@ contract ERC721EMUSK is Ownable, IERC721, ERC721Burnable, ERC721Base {
      */
     function _mint(address to, uint256 tokenId) internal virtual override(ERC721, ERC721Base) {
         ERC721Base._mint(to, tokenId);
+    }
+    
+    function toEthSignedMessageHash(bytes32 hash) internal pure returns (bytes32)
+    {
+        return keccak256(
+            abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)
+        );
+    }
+
+    function encodePackedData(uint256 id) public view returns (bytes32) {
+        return keccak256(abi.encodePacked(this, id));
+    }
+
+
+    function getecrecover(uint256 id, uint8 v, bytes32 r, bytes32 s) public view returns (address) {
+        return ecrecover(toEthSignedMessageHash(keccak256(abi.encodePacked(this, id))), v, r, s);
     }
 }
